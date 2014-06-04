1d947462cec1693c15e6c7ffd66437077398e6d2
Maros Kollar <maros@k-1.com>
2014-06-05 00:02:32 +0200

    Implement basic authentication

--- lib/App/HomelyAlarm.pm
+++ lib/App/HomelyAlarm.pm
@@ -4,13 +4,15 @@ package App::HomelyAlarm {
     our $AUTHORITY = 'cpan:MAROS';
     our $VERSION = '1.00';
     
-    use MooseX::App::Simple qw(Color);
+    use MooseX::App::Simple qw(Color ConfigHome);
     
     #use Coro;
     use AnyEvent;
     use Twiggy::Server;
     use Plack::Request;
     use WWW::Twilio::API;
+    use Try::Tiny;
+    use Digest::HMAC_SHA1 qw(hmac_sha1);
     
     option 'port' => (
         is              => 'rw',
@@ -45,6 +47,12 @@ package App::HomelyAlarm {
         required        => 1,
     );
     
+    option 'secret' => (
+        is              => 'rw',
+        isa             => 'Str',
+        required        => 1,
+    );
+    
     has 'timer' => (
         is              => 'rw',
         clearer         => 'clear_timer',
@@ -68,7 +76,6 @@ package App::HomelyAlarm {
             AccountSid => $self->twilio_sid,
             AuthToken  => $self->twilio_authtoken,
         );
-        
     }
     
     
@@ -115,22 +122,33 @@ package App::HomelyAlarm {
                 if ($self->can($authen) && ! $self->$authen($req)) {
                     return _reply_error(401);
                 }
-                $self->$coderef($req);
+                _log("Handling $method");
+                
+                my $response = try {
+                    return $self->$coderef($req);
+                } catch {
+                    _log("Error processing $method: $_");
+                    return _reply_error(500)
+                }
             } else {
                 return _reply_error(404)
             }
         });
  
         $cv->recv;
+        
+        _log('End loop');
     }
     
     sub dispatch_POST_alarm_intrusion {
         my ($self,$req) = @_;
         
-        #TODO: Authenticate
+        return _reply_ok(401)
+            unless $self->authenticate_alarm();
         
         unless ($self->has_timer) {
             $req->input->read( my $buffer, $req->header("Content-Length"), 0 );
+            _log("Set alarm intrusion timer: $buffer");
             $self->timer(AnyEvent->timer( 
                 after   => $self->alarmtimer, 
                 cb      => sub { $self->run_alarm($buffer) }
@@ -143,22 +161,24 @@ package App::HomelyAlarm {
     sub dispatch_POST_alarm_reset {
         my ($self,$req) = @_;
         
-        #TODO: Authenticate
+        return _reply_ok(401)
+            unless $self->authenticate_alarm($req);
+        
+        _log("Reset alarm intrusion timer");
         
         $self->clear_timer();
-        _reply_ok();
+        return _reply_ok();
     }
     
     sub dispatch_POST_alarm_run {
         my ($self,$req) = @_;
         
-        #TODO: Authenticate
+        return _reply_ok(401)
+            unless $self->authenticate_alarm($req);
         
         $req->input->read( my $buffer, $req->header("Content-Length"), 0 );
-        $self->timer(AnyEvent->timer( 
-            after   => 1, 
-            cb      => $self->run_alarm($buffer)
-        ));
+        _log("Run immediate alarm: $buffer");
+        $self->run_alarm($buffer);
         
         _reply_ok();     
     }
@@ -166,17 +186,20 @@ package App::HomelyAlarm {
     sub dispatch_POST_call_fallback {
         my ($self,$req) = @_;
         
-        # TODO failed call, send sms instead
-    }
-    
-    sub dispatch_POST_call_stop {
-        my ($self,$req) = @_;
+        return _reply_ok(401)
+            unless $self->authenticate_call($req);
         
-        # TODO failed call, send sms instead
+        my $message = $self->message || 'Unknown reason';
+        
+        _log("Call failed");
+        
     }
     
     sub dispatch_GET_call_twiml {
         my ($self,$req) = @_;
+        
+        return _reply_ok(401)
+            unless $self->authenticate_call($req);
         
         my $message = $self->message || 'Unknown reason';
         return [
@@ -187,9 +210,6 @@ package App::HomelyAlarm {
 <Response>
     <Say voice="woman" language="en-US">Alarm triggered!</Say>
     <Say voice="woman" language="en-US">$message</Say>
-    <Gather timeout="10" finishOnKey="1" numDigits="1" action="/call/stop" method="POST">
-        <Say>Press 1 to stop alarm.</Say>
-    </Gather>
     <Hangup/>
 </Response>
 TWIML
@@ -201,42 +221,49 @@ TWIML
         my ($self,$message) = @_;
         $self->clear_timer();
         $self->clear_message();
+        $self->message($message);
         
         _log("Running alarm");
         
-        #
-            $self->twilio->POST( 
-                'Calls',
-                From            => $self->caller_number,
-                To              => $self->callee_number,
-                Url             => $self->self_url('/call/twiml'),
-                Method          => 'GET',
-                FallbackUrl     => $self->self_url('/call/fallback'),
-                FallbackMethod  => 'POST',
-                Record          => 'false',
-                Timeout         => 60,
-            );
-        #};
-        
-        # TODO notify email/phone
+        $self->twilio->POST( 
+            'Calls',
+            From            => $self->caller_number,
+            To              => $self->callee_number,
+            Url             => $self->self_url('/call/twiml'),
+            Method          => 'GET',
+            FallbackUrl     => $self->self_url('/call/fallback'),
+            FallbackMethod  => 'POST',
+            Record          => 'false',
+            Timeout         => 60,
+        );
     } 
     
     sub authenticate_alarm {
-        my ($self) = @_;
-        unless ($req->header('Authorization') 
-            && authenticate($req->header('Authorization'))) {
-            # TODO
+        my ($self,$req) = @_;
+        my $auth = $req->header('Authorization');
+        
+        unless (defined $auth
+            && $auth eq $self->secret) {
+            _log('Could not authenticate alarm');
             return 0;
         }
         return 1;
     }
     
     sub authenticate_call {
-        my ($self) = @_;
-        unless ($req->param('AccountSid') 
-            && $req->param('AccountSid') eq $self->twilio_sid) {
+        my ($self,$req) = @_;
+        my $sid         = $req->param('AccountSid');
+        my $signature   = $req->param('X-Twilio-Signature');
+        my $digest      = hmac_sha1($req->uri, $self->twilio_authtoken);
+        
+        unless (defined $sid
+            && $sid eq $self->twilio_sid
+            && defined $signature
+            && $signature eq $digest) {
+            _log('Could not authenticate call');
             return 0;
         }
+        
         return 1;
     }
     
