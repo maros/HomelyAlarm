790ee7c97228e7be2aeb4f82590856219887ba56
Maros Kollar <maros@k-1.com>
2014-07-28 07:32:41 +0200

    Message log

---
+++ lib/App/HomelyAlarm/MessageLog.pm
@@ -0,0 +1,27 @@
+package App::HomelyAlarm::MessageLog {
+    use 5.014; 
+
+    use Moose;
+    
+    has 'message' => (
+        is          => 'ro',
+        isa         => 'Str',
+        required    => 1,
+    );
+    
+    has 'timestamp' => (
+        is          => 'ro',
+        isa         => 'Int',
+        default     => sub { time },
+    );
+    
+    has 'mode' => (
+        is          => 'ro',
+        isa         => 'Str',
+        required    => 1,
+    );
+    
+    __PACKAGE__->meta->make_immutable;
+}
+
+1;
--- lib/App/HomelyAlarm/Recipient.pm
+++ lib/App/HomelyAlarm/Recipient.pm
@@ -4,6 +4,33 @@ package App::HomelyAlarm::Recipient {
     use Moose;
     with qw(App::HomelyAlarm::Role::Recipient);
     
+    use App::HomelyAlarm::MessageLog;
+    
+    has 'message_log' => (
+        default     => sub { [] },
+        is          => 'rw',
+    );
+    
+    sub add_message {
+        my ($self,$message,$mode) = @_;
+        
+        my $message_log = App::HomelyAlarm::MessageLog->new(
+            message     => $message,
+            mode        => $mode,
+        );
+        
+        push(@{$self->message_log},$message_log);
+    }
+    
+    sub all_messages {
+        my ($self) = @_;
+        return sort { $a->timestamp <=> $b->timestamp }  @{$self->message_log};
+    }
+    
+    sub last_message {
+        my ($self) = @_;
+        return ($self->all_messages)[-1];
+    }
 }
 
 1;
