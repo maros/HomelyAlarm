20b6730050ea4f35ba8862bd4da4dd586cf67a12
Maros Kollar <maros@k-1.com>
2014-07-28 07:31:34 +0200

    Manage recipients list

--- lib/App/HomelyAlarm.pm
+++ lib/App/HomelyAlarm.pm
@@ -8,6 +8,45 @@ package App::HomelyAlarm {
     app_namespace 'App::HomelyAlarm::Command';
     
     use App::HomelyAlarm::Recipient;
+    use Sereal::Encoder qw(encode_sereal);
+    use Sereal::Decoder qw(decode_sereal);
+    
+    option 'recipients_database' => (
+        is              => 'ro',
+        required        => 1,
+        cmd_flag        => 'recipients',
+        default         => $ENV{HOME}.'/.homely_alarm.db',
+        isa             => 'Str',
+        documentation   => q[Recipients database file],
+    );
+    
+    has 'recipients' => (
+        is          => 'rw',
+        lazy_build  => 1,
+        traits      => ['Array'],
+        handles     => {
+            'add_recipient'     => 'push',
+            'recipients_list'   => 'elements',
+        }
+    );
+    
+    sub _build_recipients {
+        my ($self) = @_;
+        my $file = Path::Class::File->new($self->recipients_database);
+        return []
+            unless -e $file;
+        my $decoded = decode_sereal($file->slurp());
+        return []
+            unless ref($decoded) eq 'ARRAY';
+        return $decoded;
+    }
+    
+    sub write_recipients {
+        my ($self) = @_;
+        Path::Class::File
+            ->new($self->recipients_database)
+            ->spew(encode_sereal($self->recipients));
+    }
     
     __PACKAGE__->meta->make_immutable;
 }
